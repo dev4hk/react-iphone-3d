9bb6d6590ba1a12d0fcdbc7109856fae0b0828c1
Hoon <hoon0661@gmail.com>
2025-03-25 09:13:08 -0400

    added initial structure of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlight />
+    </main>
+  );
 };
 
 export default App;
