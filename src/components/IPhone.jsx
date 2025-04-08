d1e5321b4fbcd7e45464f1319446d5b53ecfe139
Hoon <hoon0661@gmail.com>
2025-04-07 21:46:12 -0400

    added dependency - gh pages

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -11,7 +11,7 @@ import { useGLTF, useTexture } from "@react-three/drei";
 import * as THREE from "three";
 
 function Model(props) {
-  const { nodes, materials } = useGLTF("/models/scene.glb");
+  const { nodes, materials } = useGLTF("./models/scene.glb");
   const texture = useTexture(props.item.img);
   useEffect(() => {
     Object.entries(materials).map((material) => {
@@ -254,4 +254,4 @@ function Model(props) {
 
 export default Model;
 
-useGLTF.preload("/models/scene.glb");
+useGLTF.preload("./models/scene.glb");
